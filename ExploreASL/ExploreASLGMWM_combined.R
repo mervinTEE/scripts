0b0f07b3402411b57f05334cb3372db673b17b59
Mervin <mervintee@gmail.com>
2023-11-01 16:10:23 +0800

    updated with m0 concat.

--- ExploreASL/ExploreASLGMWM_combined.R
+++ ExploreASL/ExploreASLGMWM_combined.R
@@ -1,10 +1,14 @@
-dir <- "E:/NEURO_ASL/derivatives/ExploreASL/Population/Stats"
+library(dplyr)
+library(tidyverse)
 
-setwd("E:/NEURO_ASL/derivatives/ExploreASL/Population/Stats")
+dir <- "C:/Users/Mervin/Desktop/Analysis/NEURO-BMC/Determinants/Determinants/Data/ASL"
+
+setwd("C:/Users/Mervin/Desktop/Analysis/NEURO-BMC/Determinants/Determinants/Data/ASL")
 
 
 
-# Setting patterns to recognize
+
+# CBF -------------------------------------------------------------------------------------------------------------
 GMCBF_PVC0 <- "mean_qCBF_StandardSpace_TotalGM.*PVC0\\.tsv"
 WMCBF_PVC0 <- "mean_qCBF_StandardSpace_DeepWM.*PVC0\\.tsv"
 GMCBF_PVC2 <- "mean_qCBF_StandardSpace_TotalGM.*PVC2\\.tsv"
@@ -57,6 +61,7 @@ combinedcbf<-left_join(GMWM_pvc0_cbfraw, GMWM_pvc2_cbfraw, by =  c("participant_
                                                                    "GM_vol", "WM_vol", "CSF_vol", "GM_ICVRatio", "GMWM_ICVRatio",
                                                                    "WMH_vol", "WMH_count", "MeanMotion"))
 
+GMWM_pvc2_cbfraw
 
 # ATT -------------------------------------------------------------------------------------------------------------
 GMATT_PVC0 <- "mean_ATT_StandardSpace_TotalGM.*PVC0\\.tsv"
@@ -170,17 +175,73 @@ combinedtex<-left_join(GMWM_pvc0_texraw, GMWM_pvc2_texraw, by =  c("participant_
                                                                    "GM_vol", "WM_vol", "CSF_vol", "GM_ICVRatio", "GMWM_ICVRatio",
                                                                    "WMH_vol", "WMH_count", "MeanMotion"))
 
+# M0 --------------------------------------------------------------------------------------------------------------
+GMM0_PVC0 <- "mean_M0_StandardSpace_TotalGM.*PVC0\\.tsv"
+WMM0_PVC0 <- "mean_M0_StandardSpace_DeepWM.*PVC0\\.tsv"
+GMM0_PVC2 <- "mean_M0_StandardSpace_TotalGM.*PVC2\\.tsv"
+WMM0_PVC2 <- "mean_M0_StandardSpace_DeepWM.*PVC2\\.tsv"
+
+# Listing files in directory
+files <- list.files(dir, pattern = "\\.tsv$")
+
+# Looking for files that match the patterns
+GM_PVC0_M0file <- grep(GMM0_PVC0, files, value = TRUE)
+WM_PVC0_M0file <- grep(WMM0_PVC0, files, value = TRUE)
+GM_PVC2_M0file <- grep(GMM0_PVC2, files, value = TRUE)
+WM_PVC2_M0file <- grep(WMM0_PVC2, files, value = TRUE)
+
+# Loading data from the matching files into separate data frames (df)
+GM_pvc0_M0raw <- do.call(rbind, lapply(GM_PVC0_M0file, read.table, sep = "\t", header = TRUE))
+WM_pvc0_M0raw <- do.call(rbind, lapply(WM_PVC0_M0file, read.table, sep = "\t", header = TRUE)) %>% select(participant_id, session, DeepWM_B, DeepWM_L, DeepWM_R)
+GM_pvc2_M0raw <- do.call(rbind, lapply(GM_PVC2_M0file, read.table, sep = "\t", header = TRUE))
+WM_pvc2_M0raw <- do.call(rbind, lapply(WM_PVC2_M0file, read.table, sep = "\t", header = TRUE)) %>% select(participant_id, session, DeepWM_B, DeepWM_L, DeepWM_R)
+
+# Perform the left join on GM and WM data frames
+GMWM_pvc0_M0raw <- left_join(GM_pvc0_M0raw, WM_pvc0_M0raw, by = c("participant_id", "session"))
+GMWM_pvc2_M0raw <- left_join(GM_pvc2_M0raw, WM_pvc2_M0raw, by = c("participant_id", "session"))
+
+
+# Define the suffix and prefix based on file name conditions(PVC0)
+filelist<- GM_PVC0_M0file
+suffix <- ifelse(grepl("PVC0", filelist), "_PVC0", ifelse(grepl("PVC2", filelist), "_PVC2", ""))
+prefix <- ifelse(grepl("CBF", filelist), "cbf_", ifelse(grepl("ATT", filelist), "att_", ifelse(grepl("Tex", filelist), "tex_", ifelse(grepl("M0", filelist), "m0_", ""))))
+
+GMWM_pvc0_M0raw <- GMWM_pvc0_M0raw %>%
+        rename_with(~ gsub("TotalGM", paste0("TotalGM", suffix), .), .cols = contains("TotalGM")) %>%
+        rename_with(~ gsub("DeepWM", paste0("DeepWM", suffix), .), .cols = contains("DeepWM")) %>%
+        rename_with(~ gsub("^", prefix, .), .cols = matches("TotalGM|DeepWM"))
+GMWM_pvc0_M0raw
+
+
+# Define the suffix and prefix based on file name conditions(PVC2)
+filelist<- GM_PVC2_M0file
+suffix <- ifelse(grepl("PVC0", filelist), "_PVC0", ifelse(grepl("PVC2", filelist), "_PVC2", ""))
+prefix <- ifelse(grepl("CBF", filelist), "cbf_", ifelse(grepl("ATT", filelist), "att_", ifelse(grepl("Tex", filelist), "tex_", ifelse(grepl("M0", filelist), "m0_", ""))))
+
+GMWM_pvc2_M0raw <- GMWM_pvc2_M0raw %>%
+        rename_with(~ gsub("TotalGM", paste0("TotalGM", suffix), .), .cols = contains("TotalGM")) %>%
+        rename_with(~ gsub("DeepWM", paste0("DeepWM", suffix), .), .cols = contains("DeepWM")) %>%
+        rename_with(~ gsub("^", prefix, .), .cols = matches("TotalGM|DeepWM"))
+GMWM_pvc2_M0raw
+
+combinedm0<-left_join(GMWM_pvc0_M0raw, GMWM_pvc2_M0raw, by =  c("participant_id", "session", "LongitudinalTimePoint", "SubjectNList", "Site",
+                                                                   "GM_vol", "WM_vol", "CSF_vol", "GM_ICVRatio", "GMWM_ICVRatio",
+                                                                   "WMH_vol", "WMH_count", "MeanMotion"))
+
+
 
 # Combing all Data  -----------------------------------------------------------------------------------------------
 combinedcbfatt<-left_join(combinedcbf, combinedatt, by =  c("participant_id", "session", "LongitudinalTimePoint", "SubjectNList", "Site",
                                                                    "GM_vol", "WM_vol", "CSF_vol", "GM_ICVRatio", "GMWM_ICVRatio",
                                                                    "WMH_vol", "WMH_count", "MeanMotion"))
 textemp<- combinedtex %>% select(participant_id, session, contains("Total"), contains("Deep"))
+m0temp<- combinedm0 %>% select(participant_id, session, contains("Total"), contains("Deep"))
 
-combinedall<-left_join(combinedcbfatt, textemp, by =  c("participant_id", "session"))
+combinedcbfatttex<-left_join(combinedcbfatt, textemp, by =  c("participant_id", "session"))
+combinedall<-left_join(combinedcbfatttex, m0temp, by =  c("participant_id", "session"))
 combinedall <- combinedall[-1, , drop = FALSE]
-skim(combinedall)
 
+names(combinedall)
 
 write.csv(x = combinedall, "CBFATTTEX_values.csv", row.names=FALSE)
 
