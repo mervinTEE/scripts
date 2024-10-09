85fc8cca59c13d1594d089860808d1d2ff456eaa
merveet <mervintee@gmail.com>
2024-10-09 21:58:43 +0800

    added LST scripts.

---
+++ LST-AI/LST-AI_segmentation.sh
@@ -0,0 +1,10 @@
+# Setting Root Folder
+ROOTFOLDER=/mnt/hdd/MT/HARMY/HARMY_BL_WMH
+
+#THIS COMMAND USES MRTrix3 for_each command, make sure to have MRTrix3 installed
+
+#For Segmentation and annotation from scratch, with 4 parallel loops runnings concurrently using 4 thread for each loop, change -nthreads for more loops, --threads for more cores per loop.
+for_each -nthreads 4 -info $ROOTFOLDER/rawdata/* : lst --t1 IN/NAME_T1w.nii.gz --flair IN/rmNAME_FLAIR.nii.gz --output $ROOTFOLDER/Output/NAME --temp $ROOTFOLDER/temp/NAME --existing_seg $ROOTFOLDER/corrected/ples_lga_0.3_rmNAME_WMH_corrected.nii.gz --device 0 --threads 4
+
+#For Annotation only with existing LST segmentation
+for_each -nthreads 4 -info $ROOTFOLDER/rawdata/* : lst --t1 IN/NAME_T1w.nii.gz --flair IN/rmNAME_FLAIR.nii.gz --output $ROOTFOLDER/Output/NAME --temp $ROOTFOLDER/temp/NAME --existing_seg $ROOTFOLDER/corrected/ples_lga_0.3_rmNAME_WMH_corrected.nii.gz --device 0 --annotate_only --threads 4
